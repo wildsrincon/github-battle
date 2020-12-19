4622aca8eddd3169c46cbcc3da86d194fc84a145
Wilder Rincon <wildsrincon.developer@gmail.com>
2020-12-19 17:10:21 -0500

    Adding prop-types popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,53 +1,55 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function LanguagesNav ({ selected, onUpdateLanguage }) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+
   return (
     <ul className='flex-center'>
-        {languages.map((language) => (
-          <li key={ language }>
-            <button 
-              className='btn-clear nav-link'
-              style={ language === selected ? { color: 'rgb(187, 46, 31)' } : null }
-              onClick={() => onUpdateLanguage(language)}>
-              { language }
-            </button>
-          </li>
-        ))}
-      </ul>
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className='btn-clear nav-link'
+            style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
+            onClick={() => onUpdateLanguage(language)}>
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
   )
 }
 
-export default class Popular extends React.Component {
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
+}
 
+export default class Popular extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       selectedLanguage: 'All'
     }
 
     this.updateLanguage = this.updateLanguage.bind(this)
-
   }
-
-  updateLanguage(selectedLanguage) {
+  updateLanguage (selectedLanguage) {
     this.setState({
       selectedLanguage
     })
   }
-
   render() {
     const { selectedLanguage } = this.state
 
     return (
       <React.Fragment>
-        <LanguagesNav 
-          selected={ selectedLanguage }
-          onUpdateLanguage={ this.updateLanguage }
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
         />
       </React.Fragment>
     )
-
-    
   }
 }
