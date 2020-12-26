84ded8e2d7d76753a59dcbe81de85f8643945126
Wilder Rincon <1588@holbertonschool.com>
2020-12-26 18:48:47 -0500

    Creating Results UI

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import * as React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos } from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
+import Card from './Card'
 
-function LanguagesNav ({ selected, onUpdateLanguage }) {
+function LangaugesNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
   return (
@@ -22,7 +23,7 @@ function LanguagesNav ({ selected, onUpdateLanguage }) {
   )
 }
 
-LanguagesNav.propTypes = {
+LangaugesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired
 }
@@ -30,43 +31,39 @@ LanguagesNav.propTypes = {
 function ReposGrid ({ repos }) {
   return (
     <ul className='grid space-around'>
-      { repos.map((repo, index) => {
-        const { name, owner, html_ulr, stargazers_count, forks, open_issues } = repo
+      {repos.map((repo, index) => {
+        const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
         const { login, avatar_url } = owner
 
         return (
-          <li key={html_ulr, index} className='repo bg-light'>
-            <h4 className='header-lg center-text'>
-              #{ index + 1 }
-            </h4>
-            <img 
-              className='avatar'
-              src={ avatar_url }
-              alt={ `Avatar for ${login}` }
-            />
-            <h2 className='center-text'>
-              <a className='link' href={html_ulr}>{ login }</a>
-            </h2>
-            <ul className='card-list'>
-              <li>
-                <FaUser color='rgb(255, 191, 116)' size={22} />
-                <a href={`https://github.com/${login}`}>
-                  { login }
-                </a>
-              </li>
-              <li>
-                <FaStar color='rgb(255, 215, 0)' size={22} />
-                {stargazers_count.toLocaleString()} stars
-              </li>
-              <li>
-                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
-                {forks.toLocaleString()} forks
-              </li>
-              <li>
-                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
-                {open_issues.toLocaleString()} open
-              </li>
-            </ul>
+          <li key={html_url}>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className='card-list'>
+                <li>
+                  <FaUser color='rgb(255, 191, 116)' size={22} />
+                  <a href={`https://github.com/${login}`}>
+                    {login}
+                  </a>
+                </li>
+                <li>
+                  <FaStar color='rgb(255, 215, 0)' size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                  {open_issues.toLocaleString()} open
+                </li>
+              </ul>
+            </Card>
           </li>
         )
       })}
@@ -85,21 +82,19 @@ export default class Popular extends React.Component {
     this.state = {
       selectedLanguage: 'All',
       repos: {},
-      error: null
+      error: null,
     }
 
     this.updateLanguage = this.updateLanguage.bind(this)
     this.isLoading = this.isLoading.bind(this)
   }
-
-  componentDidMount() {
+  componentDidMount () {
     this.updateLanguage(this.state.selectedLanguage)
   }
-
   updateLanguage (selectedLanguage) {
     this.setState({
       selectedLanguage,
-      error: null
+      error: null,
     })
 
     if (!this.state.repos[selectedLanguage]) {
@@ -121,25 +116,26 @@ export default class Popular extends React.Component {
         })
     }
   }
-
   isLoading() {
     const { selectedLanguage, repos, error } = this.state
 
     return !repos[selectedLanguage] && error === null
   }
-
   render() {
     const { selectedLanguage, repos, error } = this.state
 
     return (
       <React.Fragment>
-        <LanguagesNav
+        <LangaugesNav
           selected={selectedLanguage}
           onUpdateLanguage={this.updateLanguage}
         />
-        { this.isLoading() && <p>LOADING...</p> }
-        { error && <p>{ error }</p> }
-        { repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]}></ReposGrid> }
+
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p className='center-text error'>{error}</p>}
+
+        {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
       </React.Fragment>
     )
   }
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import * as React from 'react'
 import { battle } from '../utils/api'
+import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
+import Card from './Card'
 
 export default class Results extends React.Component {
-
   constructor(props) {
     super(props)
 
@@ -13,11 +14,10 @@ export default class Results extends React.Component {
       loading: true
     }
   }
-
   componentDidMount () {
     const { playerOne, playerTwo } = this.props
 
-    battle([playerOne, playerTwo])
+    battle([ playerOne, playerTwo ])
       .then((players) => {
         this.setState({
           winner: players[0],
@@ -32,17 +32,90 @@ export default class Results extends React.Component {
         })
       })
   }
+  render() {
+    const { winner, loser, error, loading } = this.state
 
-  render(){
+    if (loading === true) {
+      return <p>LOADING</p>
+    }
+
+    if (error) {
       return (
-        <div>
-          Results
-          <pre>{ JSON.stringify(this.state, null, 2) }</pre>
-        </div>
+        <p className='center-text error'>{error}</p>
       )
+    }
+
+    return (
+      <div className='grid space-around container-sm'>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Winner'}
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {winner.profile.name}
+            </li>
+            {winner.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {winner.profile.location}
+              </li>
+            )}
+            {winner.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {winner.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {winner.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {winner.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </Card>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Loser'}
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          name={loser.profile.login}
+          href={loser.profile.html_url}
+        >
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {loser.profile.name}
+            </li>
+            {loser.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {loser.profile.location}
+              </li>
+            )}
+            {loser.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {loser.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {loser.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {loser.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </Card>
+      </div>
+    )
   }
 }
-
-/* componentDidMount () {
-  const { playerOne, p }
-} */
