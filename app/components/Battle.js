e0c4d128bb7b5fe9b7ff1bfbd7f65462757bf24c
Wilder Rincon <1588@holbertonschool.com>
2020-12-26 20:06:39 -0500

    Creating component Loading.js

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Results from './Results'
--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,8 +1,9 @@
-import * as React from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos } from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
 import Card from './Card'
+import Loading from './Loading'
 
 function LangaugesNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -131,7 +132,7 @@ export default class Popular extends React.Component {
           onUpdateLanguage={this.updateLanguage}
         />
 
-        {this.isLoading() && <p>LOADING</p>}
+        {this.isLoading() && <Loading text='Fetching Repos' />}
 
         {error && <p className='center-text error'>{error}</p>}
 
