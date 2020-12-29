de55cf5903147bb978bd22b3fe577ca78217c396
Wilder Rincon <1588@holbertonschool.com>
2020-12-28 19:58:49 -0500

    Adding Netlify client side routing

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    entry: './app/index.js',
-    output: {
+  entry: './app/index.js',
+  output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
     publicPath: '/'
-},
-    module: {
-        rules: [
-            { test: /\.(js)$/, use: 'babel-loader' },
-            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
-},
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'app/index.html'
-        })
-    ],
-    devServer: {
-        historyApiFallback: true
-    }
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    }),
+    new CopyPlugin({ patterns: [{ from : '_redirects' }] })
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
 }
